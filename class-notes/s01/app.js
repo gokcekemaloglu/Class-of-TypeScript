31865110e4dcb89217bedabaa5f05ce2e62e7acc
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-23 12:02:49 +0300

    Updated app.js and app.ts files with new variable declarations and assignments; modified tsconfig.json to change target from es2016 to ES6

--- class-notes/s01/app.js
+++ class-notes/s01/app.js
@@ -1,1 +1,4 @@
 console.log("Cohort DE09");
+var b = 5;
+var c = 2;
+b = "5";
--- class-notes/s01/app.ts
+++ class-notes/s01/app.ts
@@ -1,1 +1,16 @@
 console.log("Cohort DE09");
+
+let b = 5
+let c = 2
+
+// b = "5"  // Type 'string' is not assignable to type 'number'.
+
+let e : string = "Hello"
+let f = "World!"
+
+let g : boolean;
+let h; // any atar otomatik olarak
+
+let x, y,z : string; // tek satırda birden fazla değişkene "type" belirtemiyoruz, ilkinden sonrakileri "any" olarak kabul ediyor
+x = "Hello";
+y=5
