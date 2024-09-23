7598cf41f48fd3c00aef486e6c6c04bb73c92b6d
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-23 12:16:15 +0300

    Added type declarations and examples for variables, arrays, and type assignments in app.js and app.ts files.

--- class-notes/s01/app.js
+++ class-notes/s01/app.js
@@ -1,4 +1,29 @@
 console.log("Cohort DE09");
 var b = 5;
 var c = 2;
-b = "5";
+// b = "5"  // Type 'string' is not assignable to type 'number'.
+var e = "Hello";
+var f = "World!";
+var g;
+var h; // any atar otomatik olarak
+// let x, y,z : string; // tek satırda birden fazla değişkene "type" belirtemiyoruz, baştakileri "any" olarak kabul ediyor sonrakine asıl tip ataması yapıyor.
+// x = "Hello";
+// y=5
+// z="World!"
+// z=5 //error
+//* Arrays
+// let nums : number[] = [1, 2, "Hi"]
+// nums.push("merhaba")
+// nums.push(5)
+// nums.push(false)
+// console.log(nums);
+// nums.forEach((item)=>item.toLowerCase())
+var usernames = ["Kemal", "Mustafa"];
+usernames.push("ayşe");
+// usernames.push(5) // Argument of type 'number' is not assignable to parameter of type 'string'. //error
+var surnames = [];
+surnames.push("Harold");
+surnames.push("25");
+// type declaration 2. Yol
+var nums2 = [5, 6];
+console.log(surnames);
--- class-notes/s01/app.ts
+++ class-notes/s01/app.ts
@@ -11,6 +11,35 @@ let f = "World!"
 let g : boolean;
 let h; // any atar otomatik olarak
 
-let x, y,z : string; // tek satırda birden fazla değişkene "type" belirtemiyoruz, ilkinden sonrakileri "any" olarak kabul ediyor
-x = "Hello";
-y=5
+// let x, y,z : string; // tek satırda birden fazla değişkene "type" belirtemiyoruz, baştakileri "any" olarak kabul ediyor sonrakine asıl tip ataması yapıyor.
+// x = "Hello";
+// y=5
+// z="World!"
+// z=5 //error
+
+//* Arrays
+
+// let nums : number[] = [1, 2, "Hi"]
+
+// nums.push("merhaba")
+// nums.push(5)
+// nums.push(false)
+
+// console.log(nums);
+
+// nums.forEach((item)=>item.toLowerCase())
+
+let usernames : string[] = ["Kemal", "Mustafa"]
+usernames.push("ayşe")
+// usernames.push(5) // Argument of type 'number' is not assignable to parameter of type 'string'. //error
+
+let surnames : string[] = [];
+surnames.push("Harold")
+surnames.push("25")
+
+// type declaration 2. Yol
+let nums2 : Array<number> = [5, 6]
+
+console.log(surnames);
+
+
