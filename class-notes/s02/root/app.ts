3a3793b1e353e5af7f06054cbc12e7bd30dec7fe
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2024-09-24 13:09:47 +0300

    typescript todo example

---
+++ class-notes/s02/root/app.ts
@@ -0,0 +1,42 @@
+const input = document.getElementById("todoInput") as HTMLInputElement
+const addBtn = document.getElementById("addTodoBtn") as HTMLButtonElement
+const list = document.getElementById("todoList") as HTMLUListElement
+
+// {
+//     task: "asd",
+//     status: 0-1,
+// }
+
+const enum Status {
+    Active, //0
+    Completed //1
+}
+
+interface ITask {
+    task: string,
+    status: Status
+}
+
+let tasks:ITask[] = []
+
+const updateList = () => {
+    list.innerHTML = ''
+
+    tasks.forEach((item, index)=>{
+        const li = document.createElement("li")
+        li.textContent = item.task
+        li.classList.add(item.status === Status.Completed ? "completed" : "active")
+        list.appendChild(li)
+    })
+}
+
+addBtn.addEventListener("click",()=>{
+    const newTask = input.value.trim()
+    if (newTask) {
+        tasks.push({task:newTask, status: Status.Active})
+        console.log(tasks);
+        updateList()
+        input.value = ""        
+    }
+})
+
